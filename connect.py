5ec10335ac04569925407e672dc7bff508855109
nikosevo <nikos.evo@hotmail.com>
2023-09-13 14:29:43 +0300

    fix check_same_thread

--- connect.py
+++ connect.py
@@ -5,7 +5,7 @@ def create_connection(db_file):
     """ create a database connection to a SQLite database """
     conn = None
     try:
-        conn = sqlite3.connect(db_file)
+        conn = sqlite3.connect(db_file,check_same_thread=False)
         print("Connected to database ")
         return conn
     except Error as e:
